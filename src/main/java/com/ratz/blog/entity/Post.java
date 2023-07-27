789537b954db603d2b4f84b0c8005fbe6a15cbfa
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-07-27 12:29:18 +0000

    created post entity

---
+++ src/main/java/com/ratz/blog/entity/Post.java
@@ -0,0 +1,33 @@
+package com.ratz.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ElementCollection
+    private Set<String> images = new HashSet<>();
+}
